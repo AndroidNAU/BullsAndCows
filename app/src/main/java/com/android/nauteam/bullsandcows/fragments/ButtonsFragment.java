d0ddbf1de182f6046f32816f596b937eec655c8c
D.Shpuryk <crossmenning1@gmail.com>
2016-02-04 19:28:56 +0200

    Created FragmentButtons

---
+++ app/src/main/java/com/android/nauteam/bullsandcows/fragments/ButtonsFragment.java
@@ -0,0 +1,27 @@
+package com.android.nauteam.bullsandcows.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Intelcom on 04.02.2016.
+ */
+public class ButtonsFragment extends Fragment {
+    public ButtonsFragment() {
+        super();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
