3978657671890a1c5d65f1bcbcdeb58aabfef146
Max Chemeris <mmc10@ukr.net>
2016-02-16 01:57:41 +0200

    Class Player(test version)

---
+++ app/src/main/java/model/Player.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Maxim
+ */
+public class Player {
+    private String mName;
+    private NewGame mCurrentGame;
+    private ArrayList<Game> mAllGames;
+    
+    public String getMName()
+    {
+        return mName;
+    }
+    
+    public void setMName(String usrStr)
+    {
+        this.mName = usrStr;
+    }
+    
+    ArrayList<Game> getMAllGames()
+    {
+        return mAllGames;
+    }
+    
+    public boolean isGameStarted()
+    {
+        if(mCurrentGame != null)
+            return true;
+        else
+            return false;
+    }
+    
+    public void startNewGame()
+    {
+        mCurrentGame = (NewGame) new Game();
+    }
+    
+    public void resumeGame(String tmp)
+    {
+        mCurrentGame = (NewGame) new Game(tmp);
+    }
+    
+    public void addCurrentToHistory()
+    {
+        mAllGames.add(mCurrentGame);
+    }
+    
+    public String toMString()
+    {
+        String result = getMName();
+        result += mCurrentGame.toMString();
+        return result;
+    }
+}
