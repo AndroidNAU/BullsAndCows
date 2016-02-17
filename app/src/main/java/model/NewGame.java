93f951933bb7e6f4657f3b23a7f25bef5754fcd7
MGromov <maksim_gromov_2012@mail.ru>
2016-02-18 01:45:01 +0200

    Fix package path

--- app/src/main/java/model/NewGame.java
+++ app/src/main/java/model/NewGame.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package modelcowsandbulls;
+package model;
 
 import java.util.ArrayList;
 import javafx.util.Pair;
