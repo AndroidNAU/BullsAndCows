6115cace09108c1f8907bb2ae2ea235f6a2bcafa
Nazar <nkushenko@gmail.com>
2016-02-19 02:24:15 +0200

    Fixed "New Game" & "Scores" Button

--- app/src/main/java/com/android/nauteam/bullsandcows/MenuFragment.java
+++ app/src/main/java/com/android/nauteam/bullsandcows/MenuFragment.java
@@ -1,22 +1,25 @@
 package com.android.nauteam.bullsandcows;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 import android.view.View.OnClickListener;
 
 
 public class MenuFragment extends Fragment {
 
+    private Button mresume_button;
+    private Button mnew_game_button;
+    private Button mscores_button;
+    private Button mexit_button;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -24,19 +27,28 @@ public class MenuFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_menu, parent, false);
 
+        mresume_button = (Button) v.findViewById(R.id.resume_button);
+        mnew_game_button = (Button) v.findViewById(R.id.new_game_button);
+
+        mnew_game_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), DemoGame.class);
+                startActivity(intent);
+            }
+        });
+        mscores_button = (Button)v.findViewById(R.id.scores_button);
+        mscores_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), DemoScores.class);
+                startActivity(intent);
+            }
+        });
+        mexit_button = (Button) v.findViewById(R.id.exit_button);
+
         return v;
 
     }
 
-
-      /*  mresume_button = (Button) findViewById(R.id.resume_button);
-        mnew_game_button = (Button) findViewById(R.id.new_game_button);
-        mscores_button = (Button) findViewById(R.id.scores_button);
-        mexit_button = (Button) findViewById(R.id.exit_button);
-        */
-      /*private Button mresume_button;
-    private Button mnew_game_button;
-    private Button mscores_button;
-    private Button mexit_button;*/
 }
-
