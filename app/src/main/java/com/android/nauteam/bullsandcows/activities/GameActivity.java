1a01cd868bb8bb828fadb6c070155fa05a356e5f
MGromov <maksim_gromov_2012@mail.ru>
2016-05-16 01:20:16 +0300

    Convert to json. Write to file.

--- app/src/main/java/com/android/nauteam/bullsandcows/activities/GameActivity.java
+++ app/src/main/java/com/android/nauteam/bullsandcows/activities/GameActivity.java
@@ -1,6 +1,8 @@
 package com.android.nauteam.bullsandcows.activities;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,12 +11,18 @@ import android.support.v7.widget.Toolbar;
 import com.android.nauteam.bullsandcows.R;
 import com.android.nauteam.bullsandcows.fragments.ButtonsFragment;
 import com.android.nauteam.bullsandcows.fragments.TurnsListFragment;
+import com.android.nauteam.bullsandcows.model.Player;
 import com.android.nauteam.bullsandcows.model.Turn;
 import com.android.nauteam.bullsandcows.model.Number;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class GameActivity extends AppCompatActivity
     implements ButtonsFragment.OnNumberReadyListener {
-
+    final String filename = "saved_game";
 
         @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +44,38 @@ public class GameActivity extends AppCompatActivity
         if (turnsListFragment != null) {
 
             turnsListFragment.addTurn(new Turn( new Number(number) ));
+        }
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+    }
+
+
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        Player mPlayer = Player.getInstance();
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        String mSavedGame = gson.toJson(mPlayer.getCurrentGame());
+
+        //File file = new File(filename);
+
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(mSavedGame.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
